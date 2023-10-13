7a17089d7be8e82bcd580c0ac8c08786c9604e58
Serdar Ulutas <ulut0002@algonquinlive.com>
2023-10-13 14:28:11 -0400

    Initial Commit

---
+++ ulut0002-mad9137-assignment-1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ulut0002-mad9137-assignment-1
+//
+//  Created by Serdar Ulutas on 2023-10-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ulut0002-mad9137-assignment-1/ulut0002_mad9137_assignment_1App.swift
@@ -0,0 +1,17 @@
+//
+//  ulut0002_mad9137_assignment_1App.swift
+//  ulut0002-mad9137-assignment-1
+//
+//  Created by Serdar Ulutas on 2023-10-13.
+//
+
+import SwiftUI
+
+@main
+struct ulut0002_mad9137_assignment_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
